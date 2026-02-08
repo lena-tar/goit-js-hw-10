1a72ae33e89c6d7fabc661ff1f655271caa13129
lena-tar <taran9933@gmail.com>
2026-02-08 21:57:08 +0000

    Deploying to gh-pages from @ lena-tar/goit-js-hw-10@732b05998133439b09199687b317c74679cd285a 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,4 +1,4 @@
-import"./assets/styles-C0wZFPNr.js";import{i as r}from"./assets/vendor-BbbuE1sJ.js";const t=document.querySelector(".form"),i=s=>`data:image/svg+xml,${encodeURIComponent(s)}`,a=i(`
+import"./assets/styles-C0wZFPNr.js";import{i as r}from"./assets/vendor-BbbuE1sJ.js";const e=document.querySelector(".form"),i=s=>`data:image/svg+xml,${encodeURIComponent(s)}`,a=i(`
 <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
 xmlns="http://www.w3.org/2000/svg">
   <circle
@@ -25,5 +25,5 @@ xmlns="http://www.w3.org/2000/svg">
         stroke="white" stroke-width="1.5"
         stroke-linecap="round"/>
 </svg>
-`);t.addEventListener("submit",s=>{s.preventDefault();const o=Number(t.delay.value)*1e3,n=t.state.value;new Promise((e,c)=>{setTimeout(()=>{n==="fulfilled"?e(o):c(o)},o)}).then(e=>{r.show({class:"toast toast--success",position:"topRight",timeout:5e3,progressBar:!0,close:!0,iconUrl:a,message:`Fulfilled promise in ${e}ms`})}).catch(e=>{r.show({class:"toast toast--error",position:"topRight",timeout:5e3,progressBar:!0,close:!0,iconUrl:l,message:`Rejected promise in ${e}ms`})}),t.reset()});
+`);e.addEventListener("submit",s=>{s.preventDefault();const o=Number(e.delay.value),n=e.state.value;new Promise((t,c)=>{setTimeout(()=>{n==="fulfilled"?t(o):c(o)},o)}).then(t=>{r.show({class:"toast toast--success",position:"topRight",timeout:5e3,progressBar:!0,close:!0,iconUrl:a,message:`Fulfilled promise in ${t}ms`})}).catch(t=>{r.show({class:"toast toast--error",position:"topRight",timeout:5e3,progressBar:!0,close:!0,iconUrl:l,message:`Rejected promise in ${t}ms`})}),e.reset()});
 //# sourceMappingURL=2-snackbar.js.map
